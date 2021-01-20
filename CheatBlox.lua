59b592f621782d4fff4e2e19e81b79a2ee95eb80
ScriptingStefan <75245293+ScriptingStefan@users.noreply.github.com>
2021-01-20 19:15:28 +0200

    Update CheatBlox.lua

--- CheatBlox.lua
+++ CheatBlox.lua
@@ -599,7 +599,7 @@ local function ConsoleInfo(txt)
     rconsoleprint("[*] "..txt.."\n")
     return true
 end
-local remote_http = game:HttpGet("https://raw.githubusercontent.com/ScriptingStefan/CheatBlox/main/srs.lua", true)
+local remote_http = game:HttpGetAsync("https://raw.githubusercontent.com/ScriptingStefan/CheatBlox/main/srs.lua")
 CheatBlox.Start = function()
     repeat wait() until game and workspace and game:GetService("Players").LocalPlayer
     rconsolename("CheatBlox - Made By Stefan#6965")
