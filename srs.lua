0fcd791a55e0c51625c5083fb1e63450ccd2a797
ScriptingStefan <75245293+ScriptingStefan@users.noreply.github.com>
2021-04-04 12:08:46 +0300

    Update srs.lua

--- srs.lua
+++ srs.lua
@@ -15,7 +15,7 @@ for i,v in pairs(getconnections(game:GetService("ScriptContext").Error)) do
 end
 for i,v in pairs(getconnections(game:GetService("LogService").MessageOut)) do
     v:Disable()
-end`
+end
 local CheatBlox = game:GetObjects("rbxassetid://6580324035")[1]
 wait()
 local LP = game:GetService("Players").LocalPlayer
